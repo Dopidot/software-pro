ded2c086183923ba9aa1d4e8251b98325a64ecc9
DEYEHE <jdeyehe@gmail.com>
2020-04-26 22:59:12 +0200

    Maj login screen

--- Mobile/fitisly_admin/lib/main.dart
+++ Mobile/fitisly_admin/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:fitislyadmin/Body/LoginScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'Body/HomePageBody.dart';
+import 'Body/HomeScreen.dart';
 
 
 void main() => runApp(
@@ -21,127 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Fitisly admin'),
+      home: LoginScreen(),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  TextStyle styleOS = TextStyle(fontFamily: 'OpenSans' , fontSize: 20.0);
-  static const color = const Color(0xFF45E15F);
-  TextEditingController emailController = new TextEditingController();
-  TextEditingController pwController = new TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    String email;
-    String pw;
-
-    final emailField = TextField(
-      obscureText: false,
-      style: styleOS,
-      decoration: InputDecoration(
-          hintText: "Email",
-          border:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
-      ),
-        controller: emailController
-    );
-
-    final passwordField = TextField(
-        obscureText: true,
-        style: styleOS,
-        decoration: InputDecoration(
-            hintText: "Mot de passe",
-            border:
-            OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
-        ),
-      onSubmitted: (String pwValue) {
-        setState(() {
-          pw = pwValue;
-        });
-      },
-    );
-
-    final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: color,
-
-      child: MaterialButton(
-        // minWidth: MediaQuery.of(context).size.width,
-        //padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          printInConsole(emailController.text);
-          goToHomePage();
-        },
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: styleOS.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-        minWidth: width,
-      ),
-    );
-
-
-    return Scaffold(
-      appBar: AppBar(
-        title:  Text('Fistily Admin', style: styleOS),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(1.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: width*0.2,
-                  child: Image.asset(
-                    "assets/logo.png",
-                    // fit: BoxFit.contain,
-                  ),
-                ),
-                SizedBox(height: 35.0),
-                emailField,
-                SizedBox(height: 15.0),
-                passwordField,
-                SizedBox(height: 25.0),
-                loginButon,
-                SizedBox(height: 15.0,width: width,),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-
-  void goToHomePage(){
-    Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) {
-      return new HomePage('Accueil');
-    }));
-
-  }
-
-  void printInConsole(String email){
-    print(email);
-  }
-
-
 }
