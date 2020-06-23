4d453d250dea7b593370df707b0b1be2e608ad44
Mickael Moreira <dopidot@gmail.com>
2020-06-23 22:21:48 +0200

    Back-end : Solve CORS Origin issue

--- Back-end/Admin-API/src/server.ts
+++ Back-end/Admin-API/src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 
 import swaggerRouter from './routes/swagger.route';
 import userRouter from './routes/user.route';
@@ -10,13 +9,6 @@ import videoRouter from './routes/video.route';
 
 export default class Server {
     readonly port: number;
-    options: cors.CorsOptions = {
-        allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
-        credentials: true,
-        methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-        origin: process.env.API_URL,
-        preflightContinue: false
-    };
 
     constructor(port: number) {
         this.port = port;
@@ -28,7 +20,13 @@ export default class Server {
         //middlewares
         app.use(express.json());
         app.use(express.urlencoded({extended: false}));
-        app.use(cors(this.options));
+		
+        app.use(function(req, res, next) {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
+        });
+		
         app.use('/api', swaggerRouter);
         app.use('/api/users', userRouter);
         app.use('/api/exercises', exerciseRouter);
