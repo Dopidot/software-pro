a2e2c31aa7a078e047a0cad7192dc40b8153da50
Guillaume Tako <guillaumetako@yahoo.fr>
2020-07-14 03:29:09 +0200

    Backend -> server

--- Front-end/src/api.config.ts
+++ Front-end/src/api.config.ts
@@ -1,5 +1,5 @@
 export class ApiConfig {
-    public adminApiUrl = 'http://localhost:4000';
+    public adminApiUrl = 'http://ec2-3-14-244-98.us-east-2.compute.amazonaws.com';
     public fitislyApiUrl = 'http://51.178.16.171:8150';
 
     constructor() {}
